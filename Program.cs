ae7eb642d1afae5259ec4ac445892d216e0b0bbf
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-11 11:36:17 -0400

    Read CSV file line by line & return array of values

--- Program.cs
+++ Program.cs
@@ -14,13 +14,8 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
-                                   
+            var fileContents = ReadSoccerResults(fileName);
+
         }
 
         public static string ReadFile(string fileName)
@@ -29,6 +24,21 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+            }
+            return soccerResults;
         }
     }
 }
