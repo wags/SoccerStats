56de0b227fd264d9227006a2d7bbf5b951b234dd
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-11 11:01:26 -0400

    Read a string from a text file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+
+            //var files = directory.GetFiles("*.txt");
+            //foreach(var file in files)
+            //{
+            //    Console.WriteLine(file.Name);
+            //}
+
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+            var file = new FileInfo(fileName);
+            if (file.Exists)
+            {
+                // This will automatically close the file when the program is done with it
+                using (var reader = new StreamReader(file.FullName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());
+                }
+
+                Console.ReadLine();
+          
+            }
+                        
         }
     }
 }
